497a96431ea0cda179c4b6483d6c5f8924c220fa
MartynasTumas <martynas.tumas@gmail.com>
2017-05-29 11:03:31 +0200

    login

--- src/GUI/Login/newLogin_GUI.java
+++ src/GUI/Login/newLogin_GUI.java
@@ -52,6 +52,22 @@ public class newLogin_GUI extends JFrame {
                 loginController.login(username);
 
         if(found) {
+            try {
+                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+                }
+            } catch (ClassNotFoundException ex) {
+                java.util.logging.Logger.getLogger(SystemFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                java.util.logging.Logger.getLogger(SystemFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                java.util.logging.Logger.getLogger(SystemFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+                java.util.logging.Logger.getLogger(SystemFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
             this.setVisible(false);
             MakeSound welcome = new MakeSound();
             welcome.playSound("src/welcome.wav");
